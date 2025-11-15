abf38bc8a40580143f22c579f8f5087a8bcc6baa
Rupesh <rupesh0101@hotmail.com>
2025-11-15 09:34:35 +0530

    base commit

---
+++ src/test/java/com/indeed/IndeedTest.java
@@ -0,0 +1,18 @@
+package com.indeed;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IndeedTest {
+
+	public static void main(String[] args) {
+		WebDriver wd = new ChromeDriver();
+
+	}
+
+}
